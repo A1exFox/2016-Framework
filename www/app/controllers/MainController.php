dda6667f64dc0ee4f5cffc55db18813774ca0f29
A1exFox <alex.fox.personal@gmail.com>
2024-09-24 16:42:11 +0300

    #29 multi-language part 4, lang_code inside posts DB

--- www/app/controllers/MainController.php
+++ www/app/controllers/MainController.php
@@ -2,6 +2,7 @@
 namespace app\controllers;
 
 use app\models\Main;
+use fw\core\App;
 use fw\libs\Pagination;
 use R;
 use fw\core\base\View;
@@ -13,14 +14,16 @@ class MainController extends AppController
     {
 //        new Main();
 
-        $total  = R::count('posts');
+        $lang = App::$app->getProperty('lang')['code'];
+        $total  = R::count('posts', "lang_code = ?", [$lang]);
+//        debug($total);
         $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
-        $perpage = 1;
+        $perpage = 2;
 
         $pagination = new Pagination($page, $perpage, $total);
         $start = $pagination->getStart();
 
-        $posts = R::findAll('posts', "LIMIT $start, $perpage");
+        $posts = R::findAll('posts', "lang_code = ? LIMIT $start, $perpage", [$lang]);
         View::setMeta('Blog | Index', 'Description text', 'Keywords set');
         $this->set(compact('posts', 'pagination', 'total'));
     }
